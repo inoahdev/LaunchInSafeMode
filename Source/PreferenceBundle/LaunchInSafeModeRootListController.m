a5d0b8e26953bae4a94d795c671ca2241d2f2f9a
inoahdev <inoahdev@gmail.com>
2017-05-23 23:20:30 -0700

    Add a github and twitter link to the preference bundle

--- Source/PreferenceBundle/LaunchInSafeModeRootListController.m
+++ Source/PreferenceBundle/LaunchInSafeModeRootListController.m
@@ -16,4 +16,39 @@
 
     return _specifiers;
 }
+
+- (void)openURL:(NSURL *)url {
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+    UIApplication *application = [UIApplication sharedApplication];
+    if ([application respondsToSelector:@selector(openURL:options:completionHandler:)]) {
+        [application openURL:url options:@{} completionHandler:nil];
+    } else {
+        [application openURL:url];
+    }
+
+    #pragma clang diagnostic pop
+}
+
+- (void)twitter {
+    @autoreleasepool {
+        UIApplication *application = [UIApplication sharedApplication];
+        if ([application canOpenURL:[NSURL URLWithString:@"tweetbot:"]]) {
+            [self openURL:[NSURL URLWithString:@"tweetbot:///user_profile/inoahdev"]];
+        } else if ([application canOpenURL:[NSURL URLWithString:@"twitterrific:"]]) {
+            [self openURL:[NSURL URLWithString:@"twitterrific://user?screen_name=inoahdev"]];
+        } else if ([application canOpenURL:[NSURL URLWithString:@"twitter:"]]) {
+            [self openURL:[NSURL URLWithString:@"twitter://user?screen_name=inoahdev"]];
+        } else {
+            [self openURL:[NSURL URLWithString:@"https://twitter.com/inoahdev"]];
+        }
+    }
+}
+
+- (void)github {
+    @autoreleasepool {
+        [self openURL:[NSURL URLWithString:@"https://github.com/inoahdev/LaunchInSafeMode"]];
+    }
+}
 @end
